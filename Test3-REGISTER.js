419af84d3ef4a45421ac20d9938c177a652317ba
matheusfelix75 <83690394+matheusfelix75@users.noreply.github.com>
2021-08-26 23:00:06 +0700

    Update Test3-REGISTER.js

--- Test3-REGISTER.js
+++ Test3-REGISTER.js
@@ -1,14 +1,14 @@
 describe('kumpulan1', function(){
     it ('LOGIN', function(){
-    cy.URL()
+        cy.URL()
     
-    cy.visit('https://demoblaze.com/')
-    cy.get('#signin2').click()
-    cy.wait(1000)
-    cy.get('#sign-username').type('REGISTERBARU4')
-    cy.get('#sign-password').type('test123')
-    cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
-    cy.wait(1000)
+        cy.visit('https://demoblaze.com/')
+        cy.get('#signin2').click()
+        cy.wait(1000)
+        cy.get('#sign-username').type('REGISTERBARU01')
+        cy.get('#sign-password').type('test123')
+        cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+        cy.wait(1000)
     })
     
 })
